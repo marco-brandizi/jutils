1ddc09451053e06aff49edc44160340f42b9b67e
Marco Brandizi <marco.brandizi@gmail.com>
2020-12-03 13:12:36 +0000

    Debugging GH Actions.

--- ci-build.sh
+++ ci-build.sh
@@ -1,5 +1,7 @@
 set -e
-if [[ `git log -1 --pretty=format:"%s"` =~ '\[ci skip\]' ]]; then
+ci_skip_tag='[ci skip]'
+
+if [[ `git log -1 --pretty=format:"%s"` =~ "$ci_skip_tag" ]]; then
 	echo -e "\n[ci skip] prefix, ignoring this commit\n"
 	exit
 fi
@@ -7,11 +9,12 @@ fi
 mvn deploy --settings maven-settings.xml  --no-transfer-progress --batch-mode
 
 if [[ ! -z "$CI_PULL_REQUEST" ]] || [[ "$CI_BRANCH" != 'master' ]]; then
+	echo "PR: '$CI_PULL_REQUEST', BRANCH: '$CI_BRANCH'"
 	echo -e "\nThis isn't main-repo/master, skipping Javadoc\n"
 	exit
 fi 
 	
 ./mk-javadocs.sh
 git remote set-url origin https://marco-brandizi:$GITHUB_TOKEN@github.com/marco-brandizi/jutils 
-git commit -a -m 'Updating auto-generated files from CI [ci skip]'
+git commit -a -m "Updating auto-generated files from CI $ci_skip_tag"
 git push origin HEAD:"$TRAVIS_BRANCH"

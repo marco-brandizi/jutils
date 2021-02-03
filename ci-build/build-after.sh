021af18abda79de2f2c8ffba5a71244b8b1b48f3
Marco Brandizi <marco.brandizi@gmail.com>
2021-02-03 16:02:20 +0000

    Using knetminer-common scripts for CI.

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -1,5 +1,5 @@
 # Local customisations injected inside the workflow of build.sh
-# See https://github.com/Rothamsted/knetminer-common for details  
+# See https://github.com/Rothamsted/knetminer-common for details. 
 #
 bash ./ci-build/mk-javadoc.sh
 git commit -a -m "Updating auto-generated files from CI $CI_SKIP_TAG"

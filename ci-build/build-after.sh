595425681db6629255b871ccc32cae56bd7cf510
Marco Brandizi <marco.brandizi@gmail.com>
2021-02-03 16:27:22 +0000

    Debugging CI scripts.

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -1,6 +1,9 @@
 # Local customisations injected inside the workflow of build.sh
 # See https://github.com/Rothamsted/knetminer-common for details. 
 #
-echo -e "\n\n\tMaking and committing Javadocs\n"
 bash ./ci-build/mk-javadocs.sh
+
+echo -e "\n\n\tCommitting Javadocs\n"
 git commit -a -m "Updating auto-generated files from CI $CI_SKIP_TAG"
+export NEEDS_PUSH=true # instructs build.sh to push the changes above
+ 

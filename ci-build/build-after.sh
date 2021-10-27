a6b6f67014d988a874f68bafd8517fc85268c2a5
Marco Brandizi <marco.brandizi@gmail.com>
2021-10-27 23:22:29 +0200

    Releasing 10.0 [ci skip]

--- ci-build/build-after.sh
+++ ci-build/build-after.sh
@@ -4,6 +4,7 @@
 bash ./ci-build/mk-javadocs.sh
 
 echo -e "\n\n\tCommitting Javadocs\n"
+git add doc
 git commit -a -m "Updating auto-generated files from CI $CI_SKIP_TAG"
 export NEEDS_PUSH=true # instructs build.sh to push the changes above
  

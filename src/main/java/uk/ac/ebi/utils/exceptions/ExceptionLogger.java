48af97467a46d08dda5fa40ed849e42d392babbd
Marco Brandizi <marco.brandizi@gmail.com>
2022-09-01 12:57:34 +0100

    ExceptionLogger, comments and minor fixes.

--- src/main/java/uk/ac/ebi/utils/exceptions/ExceptionLogger.java
+++ src/main/java/uk/ac/ebi/utils/exceptions/ExceptionLogger.java
@@ -53,13 +53,41 @@ public class ExceptionLogger
 		return getLogger ( (Logger) null );
 	}
 	
+	/**
+	 * Defaults to reporting your message (instantiated with your params), plus sensibgle tails like
+	 * ".Error: {}" for the error level entry and ".Details: {}" for the debug entry. 
+	 * 
+	 * @see #logEx(String, String, String, Throwable, Object...)
+	 * 
+	 */
 	public void logEx ( String baseMessage,Throwable ex, Object... msgParams) 
 	{
-		logEx ( baseMessage, ". Error {}", ". Details {}", ex, msgParams );
+		logEx ( baseMessage, ". Error: {}", ". Details: {}", ex, msgParams );
 	}
 	
-	public void logEx ( String baseMessage, String errorMsgTail, String debugMsgTail, Throwable ex,
-		Object... msgParams) 
+	/**
+	 * Does the exception logging, as explained above.
+	 *  
+	 * @param baseMessage A base message. This can contain '{}' placeholders as usually. If it does, they must match 
+	 *   msgParams.
+	 *   
+	 * @param errorMsgTail a tail that is used for the error level log message.
+	 *   This usually contains a last '{}' placeholder, which
+	 *   is filled with the {@link Throwable#getMessage() exception message}.
+	 *   
+	 * @param debugMsgTail a tail that is used for the debug level log. Similarly, this is filled with 
+	 *   the {@link Throwable exception itself}, so that the entire stacktrace is reported. 
+	 * 
+	 * @param ex the exception that you want to log
+	 * 
+	 * @param msgParams the parameters to be used in the message placeholders. <b>WARNING</b>: as explained above, 
+	 *   the final messsge is this plus errorMsgTail or debugMsgTail and the logger is invoked with this parameter
+	 *   extended with either the exception messsge or the exception object. So you've to consider that
+	 *   the last '{}' in the message (usually set in the tails) is for the exception, NOT for your 
+	 *   parameters.
+	 */
+	public void logEx (
+		String baseMessage, String errorMsgTail, String debugMsgTail, Throwable ex, Object... msgParams) 
 	{
 		Object [] out = new Object [ msgParams.length + 1 ];
 		
@@ -67,13 +95,17 @@ public class ExceptionLogger
 		if ( log.isDebugEnabled() )
 		{
 		   System.arraycopy ( msgParams, 0, out, 0, msgParams.length );
+
+		   if ( log.isErrorEnabled() )
+		   {
+		  	 // Usually it's cleaner to read the error level first
+			   out [ msgParams.length ] = ex.getMessage ();
+			   log.error ( baseMessage + errorMsgTail, out );
+		   }
+		   
+		   // And then the debug message
 		   out [ msgParams.length ] = ex;
 		   log.debug ( baseMessage + debugMsgTail, out );
-		   if ( !log.isErrorEnabled() ) {
-			   return;
-		   }
-		   out [ msgParams.length ] = ex.getMessage ();
-		   log.error ( baseMessage + errorMsgTail, out );
 		}
 		
 	}

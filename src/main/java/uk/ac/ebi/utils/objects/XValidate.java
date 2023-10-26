f4dd5da6aaf3a0c59f47cca251e56fd5775181c4
Marco Brandizi <marco.brandizi@gmail.com>
2023-10-26 10:45:16 +0200

    Adding XValiAdate.

---
+++ src/main/java/uk/ac/ebi/utils/objects/XValidate.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.utils.objects;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import org.apache.commons.lang3.Validate;
+
+/**
+ * Logical extension to {@link Validate}, which adds more methods. 
+ *
+ * @author brandizi
+ * <dl><dt>Date:</dt><dd>26 Oct 2023</dd></dl>
+ *
+ */
+public class XValidate
+{
+	/**
+	 * Checks that the collection isn't null and isn't empty 
+	 */
+  public static <C extends Collection<?>> C notEmpty (final C collection, final String message, final Object... values) 
+  {
+    Validate.isTrue ( collection != null && !collection.isEmpty (), message, values );
+    return collection;
+  }
+}

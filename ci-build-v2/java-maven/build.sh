c47ff443769b9fb0bce1cdfa8bf73a2a552ae7fe
Marco Brandizi <marco.brandizi@gmail.com>
2025-08-06 00:05:40 +0100

    Integrating new CI framework.

--- ci-build-v2/java-maven/build.sh
+++ ci-build-v2/java-maven/build.sh
@@ -6,7 +6,7 @@ function stage_build_setup_local
 	if [[ ! -z "$CI_SECRETS_GIST_TOKEN" ]]; then
 		printf "== Downloading Project Secrets from gist location\n"
 		gist_url="https://gist.githubusercontent.com/marco-brandizi/$CI_SECRETS_GIST_TOKEN/raw/github-brandizi-secrets.sh"
-		source <(curl -o - "$gist_url")
+		source <(curl --fail-with-body -o - "$gist_url")
 	fi
 	
 	stage_build_setup
@@ -17,7 +17,7 @@ function stage_build_local
 {
 	stage_build
 	
-	[[ "$GIT_BRANCH" == 'master' ]] || return 0
+	[[ "$CI_GIT_BRANCH" == 'master' ]] || return 0
 
 	bash ./ci-build/mk-javadocs.sh
 	

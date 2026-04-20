34fee4025701e28b7f26281c77775680a425d215
Marco Brandizi <marco.brandizi@gmail.com>
2026-04-20 16:36:04 +0100

    chore: switch the CI script pointers to the new repo

--- ci-build-v2/java-maven/build.sh
+++ ci-build-v2/java-maven/build.sh
@@ -31,7 +31,7 @@ function stage_build_local
 }
 
 printf "== Installing ci-build scripts and then running the build\n"
-ci_build_url_base="https://raw.githubusercontent.com/Rothamsted/knetminer-common/refs/heads/ci-build-v2"
+ci_build_url_base="https://raw.githubusercontent.com/KnetMiner/knetminer-ci/refs/heads/ci-build-v2"
 script_url="$ci_build_url_base/ci-build-v2/install.sh"
 . <(curl --fail-with-body -o - "$script_url") "$ci_build_url_base" java-maven
 

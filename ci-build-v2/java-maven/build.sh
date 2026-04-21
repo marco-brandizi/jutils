609674ea3bd2767d5b3e62c193fdbc6364981ab7
Marco Brandizi <marco.brandizi@gmail.com>
2026-04-21 12:06:51 +0100

    build: clean the CI launcher, point it to ci-build-v2@1.0

--- ci-build-v2/java-maven/build.sh
+++ ci-build-v2/java-maven/build.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-function stage_build_setup_local
-{
-	if [[ ! -z "$CI_SECRETS_GIST_TOKEN" ]]; then
-		printf "== Downloading Project Secrets from gist location\n"
-		gist_url="https://gist.githubusercontent.com/marco-brandizi/$CI_SECRETS_GIST_TOKEN/raw/github-brandizi-secrets.sh"
-		source <(curl --fail-with-body -o - "$gist_url")
-	fi
-	
-	stage_build_setup
-}
-
 
 function stage_build_local
 {
@@ -33,7 +22,7 @@ function stage_build_local
 printf "== Installing ci-build scripts and then running the build\n"
 
 # TODO: DO USE a version tag in place of main, DO MAKE your builds stable and predictable
-ci_build_url_base="https://raw.githubusercontent.com/KnetMiner/knetminer-ci/refs/heads/main"
+ci_build_url_base="https://raw.githubusercontent.com/KnetMiner/knetminer-ci/refs/heads/1.0"
 script_url="$ci_build_url_base/ci-build-v2/install.sh"
 . <(curl --fail-with-body -o - "$script_url") "$ci_build_url_base" java-maven
 

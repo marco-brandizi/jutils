8c835fc713222aef357221cc0e1b212cb58f7a70
Marco Brandizi <marco.brandizi@gmail.com>
2026-04-23 19:23:34 +0100

    qa (LambdaUtils): add comments and make a minor code improvement

--- src/main/java/uk/ac/ebi/utils/lambda/LambdaUtils.java
+++ src/main/java/uk/ac/ebi/utils/lambda/LambdaUtils.java
@@ -15,19 +15,29 @@ import com.machinezoo.noexception.Exceptions;
  */
 public class LambdaUtils
 {
+	/**
+	 * Turns a {@link Runnable} into a {@link Callable} that returns null. Useful as an adapter. 
+	 */
 	public static Callable<Void> toCallable ( Runnable runnable )
 	{
 		return () -> { runnable.run (); return null; };
 	}
 	
+	/**
+	 * Turns a {@link Callable} into a {@link Supplier}, including a checked exception wrapper.
+	 */
 	public static <T> Supplier<T> toSupplier ( Callable<T> callable )
 	{
 		return Exceptions.sneak ().supplier ( () -> callable.call () );
 	}
 
+	/**
+	 * Turns a {@link Runnable} into a {@link Supplier} that returns null. 
+	 * 
+	 */
 	public static Supplier<Void> toSupplier ( Runnable runnable )
 	{
-		return toSupplier ( toCallable ( runnable ) );
+		return () -> { runnable.run (); return null; };
 	}
 	
 }
